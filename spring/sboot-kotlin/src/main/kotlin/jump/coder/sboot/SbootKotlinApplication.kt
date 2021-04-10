0b93eab851643bc0ac7632e6991d4763f2edebbf
Ayrat Bik <ajratbik@gmail.com>
2021-04-11 00:25:10 +0700

    add sboot empty project and ktor http service creating template project

---
+++ spring/sboot-kotlin/src/main/kotlin/jump/coder/sboot/SbootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package jump.coder.sboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbootKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SbootKotlinApplication>(*args)
+}
---
+++ spring/sboot-kotlin/src/test/kotlin/jump/coder/sboot/SbootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package jump.coder.sboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SbootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
